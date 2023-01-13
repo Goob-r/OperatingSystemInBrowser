ce0321993f470ddda28f19e96c4dbff5916188c1
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-01-13 10:23:23 -0800

    Update webGrabber.java

--- bnb/com/webGrabber.java
+++ bnb/com/webGrabber.java
@@ -1,11 +1,11 @@
-package com.zetcode;
+package com.bnb;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class ReadWebPageEx {
+public class webGrabber {
 
     public static void main(String[] args) throws IOException {
 
