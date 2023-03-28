d5daea6189395f50e156c88e0938c7c1e12dd50f
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-03-28 18:44:18 +0000

    test edit

--- system/startUp.js
+++ system/startUp.js
@@ -10,9 +10,14 @@ const ico = "system/icons/splashIco.svg";
 try {
   const img = document.createElement('img');
   img.src = ico;
-  img.height = 100;
-  img.width = 100;
+  img.height = screen.height / 100;
+  img.width = screen.height;
   document.body.appendChild(img);
+
+  for (let i  = 1; 1 < 100; i++) {
+    setTimeout(function() {console.log("ok goober");}, 1000);
+    img.height = screen.height / (100 - i);
+  }
 } catch(err) {
   document.write(err.message);
 }
