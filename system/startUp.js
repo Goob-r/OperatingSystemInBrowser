9b3b8e120b7217b840631f9a39c494f6ff2bdd1e
FancyBoiL1 <117115118+FancyBoiL1@users.noreply.github.com>
2023-03-28 21:01:03 +0000

    Optimization

--- system/startUp.js
+++ system/startUp.js
@@ -5,15 +5,13 @@
 } else if (elem.msRequestFullscreen) {
   elem.msRequestFullscreen();
 }*/
-const ico = "system/icons/splashIco.svg";
 const console = document.createElement('p');
 document.body.appendChild(console);
 
 try {
   const img = document.createElement('img');
-  img.src = ico;
-  img.height = screen.height;
-  img.width = screen.height;
+  img.src = "system/icons/splashIco.svg";
+  img.height, img.width = screen.height;
   document.body.appendChild(img);
 } catch(err) {
   console.innerHtml = err.message;
