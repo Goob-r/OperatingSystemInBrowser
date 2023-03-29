52ae3931d57ec78161f70955934ec338178b6456
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-03-29 04:30:27 +0000

    fixings

--- system/startUp.js
+++ system/startUp.js
@@ -5,13 +5,13 @@
 } else if (elem.msRequestFullscreen) {
   elem.msRequestFullscreen();
 }*/
-const console = document.createElement('p');
-document.body.appendChild(console);
-
 try {
   const img = document.createElement('img');
   img.src = "system/icons/splashIco.svg";
-  img.height, img.width = screen.height;
+  img.height = screen.height;
+  img.width = screen.height;
+  img.style.position = "absolute";
+  img.style.left = (screen.width / 2) - (img.width / 2);
   document.body.appendChild(img);
 } catch(err) {
   console.innerHtml = err.message;
