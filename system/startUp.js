ac98ec193f22ed6cdfde81c7da31f14360a76b02
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-03-30 18:36:22 +0000

    test

--- system/startUp.js
+++ system/startUp.js
@@ -1,18 +1,35 @@
-/*if (elem.requestFullscreen) {
-  elem.requestFullscreen();
-} else if (elem.webkitRequestFullscreen) {
-  elem.webkitRequestFullscreen();
-} else if (elem.msRequestFullscreen) {
-  elem.msRequestFullscreen();
-}*/
+try {
+  if (document.body.requestFullscreen) {
+    document.body.requestFullscreen();
+  } else if (document.body.webkitRequestFullscreen) {
+    document.body.webkitRequestFullscreen();
+  } else if (document.body.msRequestFullscreen) {
+    document.body.msRequestFullscreen();
+  }
+} catch(err) {
+  document.write(err.message);
+}
+
 try {
   const img = document.createElement('img');
-  img.src = "system/icons/splashIco.svg";
-  img.height = screen.height;
-  img.width = screen.height;
+  //img.src = "system/icons/splashIco.svg";
+  img.src = "https://www.w3schools.com/css/img_lights.jpg";
   img.style.position = "absolute";
-  img.style.left = (screen.width / 2) - (img.width / 2);
+  img.height = window.innerHeight;
+  img.width = window.innerHeight;
+  img.style.left = ((window.innerWidth / 2) - (img.width / 2)) + "px";
+  img.style.top = ((window.innerHeight / 2) - (img.height / 2)) + "px";
   document.body.appendChild(img);
+  setInterval(() => {
+    let v = 1;
+    for (let i = 10; i > 0; i -= 1) {
+      setInterval(() => {
+        img.style.opacity = i / 10;
+      }, v * 100);
+      v++;
+      
+    }
+  }, 3000);
 } catch(err) {
-  console.innerHtml = err.message;
+  document.write(err.message);
 }
