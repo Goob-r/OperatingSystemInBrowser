0007cefbed2d4bf85786b6898518838317ba308c
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-03-31 11:35:10 -0700

    added the more elements
    Added the errorOutput and the taskbar startup

--- system/startUp.js
+++ system/startUp.js
@@ -15,20 +15,30 @@ try {
   //img.src = "system/icons/splashIco.svg";
   img.src = "https://www.w3schools.com/css/img_lights.jpg";
   img.style.position = "absolute";
-  img.height = img.width = window.innerHeight;
+  img.height = window.innerHeight;
+  img.width = window.innerHeight;
   img.style.left = ((window.innerWidth / 2) - (img.width / 2)) + "px";
   img.style.top = ((window.innerHeight / 2) - (img.height / 2)) + "px";
   document.body.appendChild(img);
   setInterval(() => {
-    let v = 1;
-    for (let i = 10; i > 0; i -= 1) {
-      setInterval(() => {
-        img.style.opacity = i / 10;
-      }, v * 100);
-      v++;
-      
-    }
+    img.remove
   }, 3000);
+  
+  const errorOutput = document.createElement('p');
+  errorOutput.position = "absolute";
+  errorOutput.top = "0px";
+  errorOutput.left = "0px";
+  errorOutput.id = "errorOut";
 } catch(err) {
   document.write(err.message);
+}
+
+try {
+  const taskbar = document.createElement('div');
+  taskbar.width = window.innerWidth;
+  taskbar.style.position = "absolute";
+  taskbar.style.bottom = "0px";
+  taskbar.id = "taskbar";
+} catch(err) {
+  document.getElementById("errorOut").innerHtml = err.message;
 }
