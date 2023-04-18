760b428eed6fb97ee4cec3a85978fdc0b35a6605
Goob-r <122378192+Goob-r@users.noreply.github.com>
2023-04-17 21:28:24 -0700

    fixes to getting data and start screen loads stuff now

--- system/startUp.js
+++ system/startUp.js
@@ -1,40 +1,36 @@
 try {
-  if (document.body.requestFullscreen) {
-    document.body.requestFullscreen();
-  } else if (document.body.webkitRequestFullscreen) {
-    document.body.webkitRequestFullscreen();
-  } else if (document.body.msRequestFullscreen) {
-    document.body.msRequestFullscreen();
-  }
-} catch(err) {
-  document.write(err.message);
-}
+ async function startUp() {
+   const img = document.createElement("img");
+   img.src = "system/icons/splashIco.svg";
+   img.style.position = "absolute";
+   img.height = window.innerHeight;
+   img.width = window.innerHeight;
+   img.style.left = window.innerWidth / 2 - img.width / 2 + "px";
+   img.style.top = window.innerHeight / 2 - img.height / 2 + "px";
+   document.body.appendChild(img);
+    const response = await fetch("system/sysData/systemOptions.json");
+   const systemOptions = await response.json();
 
-try {
-  const img = document.createElement('img');
-  const errorOutput = document.createElement('p');
-  //img.src = "system/icons/splashIco.svg";
-  img.src = "https://www.w3schools.com/css/img_lights.jpg";
-  errorOutput.position = img.style.position = "absolute";
-  img.height = img.width = window.innerHeight;
-  img.style.left = ((window.innerWidth / 2) - (img.width / 2)) + "px";
-  img.style.top = ((window.innerHeight / 2) - (img.height / 2)) + "px";
-  document.body.appendChild(img);
-  setInterval(() => {
-    img.remove
-  }, 3000);
-  errorOutput.left = errorOutput.top = "0px";
-  errorOutput.id = "errorOut";
-} catch(err) {
-  document.write(err.message);
-}
 
-try {
-  const taskbar = document.createElement('div');
-  taskbar.width = window.innerWidth;
-  taskbar.style.position = "absolute";
-  taskbar.style.bottom = "0px";
-  taskbar.id = "taskbar";
-} catch(err) {
-  document.getElementById("errorOut").innerHtml = err.message;
+   var taskbarColor = systemOptions.taskbar.color;
+
+
+   const taskbar = document.createElement("div");
+   taskbar.width = window.innerWidth;
+   taskbar.style.position = "absolute";
+   /*if (systemOptions.taskbar.position == "bottom") {
+     taskbar.style.bottom = "0px";
+   } else {
+     taskbar.style.top = "0px";
+   }*/
+   /*alert("rgba(" + taskbarColor[0] + "," + taskbarColor[1] + "," + taskbarColor[2] + "," + taskbarColor[3] + ")");*/
+   taskbar.style.backgroundColor = "rgb(0, 0, 0,)";
+   document.body.appendChild(taskbar)
+    img.remove();
+ }
+
+
+ startUp();
+} catch (err) {
+ alert("WARN \nCOULD NOT START UP:" + err.message);
 }
